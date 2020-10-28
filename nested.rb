2c960dbf2d75feb2f04b22cb74d83bdbe7137c8e
Mark G <markghaida@gmail.com>
2020-10-28 04:27:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = ""
+programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
@@ -126,5 +126,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
